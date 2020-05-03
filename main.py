8b4e32dec0dbf6f0dbbdd852b536e327d58cbaf9
Jessica Wang <jessicawang225@gmail.com>
2020-05-03 00:57:27 -0700

    Finished uploading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import argparse
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -16,6 +17,7 @@ This code is adapted from two sources:
 (ii) Starter code from Yisong Yue's CS 155 Course (http://www.yisongyue.com/courses/cs155/2020_winter/)
 '''
 
+
 class fcNet(nn.Module):
     '''
     Design your model with fully connected layers (convolutional layers are not
@@ -23,6 +25,7 @@ class fcNet(nn.Module):
     are the sample units you can try:
         Linear, Dropout, activation layers (ReLU, softmax)
     '''
+
     def __init__(self):
         # Define the units that you will use in your model
         # Note that this has nothing to do with the order in which operations
@@ -48,9 +51,10 @@ class ConvNet(nn.Module):
     '''
     Design your model with convolutional layers.
     '''
+
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3,3), stride=1)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=1)
         self.conv2 = nn.Conv2d(8, 8, 3, 1)
         self.dropout1 = nn.Dropout2d(0.5)
         self.dropout2 = nn.Dropout2d(0.5)
@@ -81,11 +85,35 @@ class Net(nn.Module):
     '''
     Build the best MNIST classifier.
     '''
+
     def __init__(self):
         super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(3, 3), stride=1)
+        self.conv2 = nn.Conv2d(64, 64, 3, 1)
+        self.norm = nn.BatchNorm2d(64)
+        self.dropout1 = nn.Dropout2d(0.15)
+        self.dropout2 = nn.Dropout2d(0.15)
+        self.fc1 = nn.Linear(1600, 64)
+        self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
-        return x
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout2(x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -93,26 +121,26 @@ def train(args, model, device, train_loader, optimizer, epoch):
     This is your training function. When you call this function, the model is
     trained for 1 epoch.
     '''
-    model.train()   # Set the model to training mode
+    model.train()  # Set the model to training mode
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()               # Clear the gradient
-        output = model(data)                # Make predictions
-        loss = F.nll_loss(output, target)   # Compute loss
-        loss.backward()                     # Gradient computation
-        optimizer.step()                    # Perform a single optimization step
+        optimizer.zero_grad()  # Clear the gradient
+        output = model(data)  # Make predictions
+        loss = F.nll_loss(output, target)  # Compute loss
+        loss.backward()  # Gradient computation
+        optimizer.step()  # Perform a single optimization step
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.sampler),
-                100. * batch_idx / len(train_loader), loss.item()))
+                       100. * batch_idx / len(train_loader), loss.item()))
 
 
 def test(model, device, test_loader):
-    model.eval()    # Set the model to inference mode
+    model.eval()  # Set the model to inference mode
     test_loss = 0
     correct = 0
     test_num = 0
-    with torch.no_grad():   # For the inference step, gradient is not computed
+    with torch.no_grad():  # For the inference step, gradient is not computed
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
@@ -126,6 +154,30 @@ def test(model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, test_num,
         100. * correct / test_num))
+
+
+def train_valid_split(train_dataset):
+    np.random.random(2020)
+
+    digits = {}
+    for i in range(len(train_dataset)):
+        label = train_dataset[i][1]
+        if label in digits:
+            digits[label].append(i)
+        else:
+            digits[label] = [i]
+
+    subset_indices_train = []
+    subset_indices_valid = []
+    for digit in digits:
+        indices = digits[digit]
+        total = len(indices)
+        np.random.shuffle(indices)
+        subset_indices_valid.extend(indices[:int(0.15 * total)])
+        subset_indices_train.extend(indices[int(0.15 * total):])
+
+    assert (len(subset_indices_train) + len(subset_indices_valid) == len(train_dataset))
+    return subset_indices_train, subset_indices_valid
 
 
 def main():
@@ -158,7 +210,7 @@ def main():
 
     parser.add_argument('--save-model', action='store_true', default=True,
                         help='For Saving the current Model')
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
@@ -176,10 +228,10 @@ def main():
         model.load_state_dict(torch.load(args.load_model))
 
         test_dataset = datasets.MNIST('../data', train=False,
-                    transform=transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.1307,), (0.3081,))
-                    ]))
+                                      transform=transforms.Compose([
+                                          transforms.ToTensor(),
+                                          transforms.Normalize((0.1307,), (0.3081,))
+                                      ]))
 
         test_loader = torch.utils.data.DataLoader(
             test_dataset, batch_size=args.test_batch_size, shuffle=True, **kwargs)
@@ -190,17 +242,13 @@ def main():
 
     # Pytorch has default MNIST dataloader which loads data at each iteration
     train_dataset = datasets.MNIST('../data', train=True, download=True,
-                transform=transforms.Compose([       # Data preprocessing
-                    transforms.ToTensor(),           # Add data augmentation here
-                    transforms.Normalize((0.1307,), (0.3081,))
-                ]))
+                                   transform=transforms.Compose([  # Data preprocessing
+                                       transforms.ToTensor(),  # Add data augmentation here
+                                       transforms.Normalize((0.1307,), (0.3081,))
+                                   ]))
 
-    # You can assign indices for training/validation or use a random subset for
-    # training by using SubsetRandomSampler. Right now the train and validation
-    # sets are built from the same indices - this is bad! Change it so that
-    # the training and validation sets are disjoint and have the correct relative sizes.
-    subset_indices_train = range(len(train_dataset))
-    subset_indices_valid = range(len(train_dataset))
+    # Ensure the training and validation sets are disjoint and have the correct relative sizes
+    subset_indices_train, subset_indices_valid = train_valid_split(train_dataset)
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size,
@@ -212,7 +260,7 @@ def main():
     )
 
     # Load your model [fcNet, ConvNet, Net]
-    model = ConvNet().to(device)
+    model = Net().to(device)
 
     # Try different optimzers here [Adam, SGD, RMSprop]
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
@@ -224,12 +272,12 @@ def main():
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(model, device, val_loader)
-        scheduler.step()    # learning rate scheduler
+        scheduler.step()  # learning rate scheduler
 
         # You may optionally save your model at each epoch here
 
     if args.save_model:
-        torch.save(model.state_dict(), "mnist_model.pt")
+        torch.save(model.state_dict(), "best_MNIST_Net.pt")
 
 
 if __name__ == '__main__':
